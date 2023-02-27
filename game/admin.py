8eb4c9334280c7c0a37cff6310eb9cffb1101dcc
StanMathers <sabagamgebeli@gmail.com>
2023-02-27 11:14:16 +0400

    Added initial tables

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from game.models import Game, GameSessionMoves
+
+admin.site.register(Game)
+admin.site.register(GameSessionMoves)
+
--- game/models.py
+++ game/models.py
@@ -1,3 +1,33 @@
+import uuid
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Game(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return str(self.uuid)
+
+class GameSessionMoves(models.Model):
+    """
+    0 1 2
+    3 4 5
+    6 7 8
+    """
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.CharField(max_length=1, choices=[('X', 'X'), ('O', 'O')])
+    move = models.IntegerField(validators=[MaxValueValidator(8), MinValueValidator(0)])
+    
+    winner = models.CharField(max_length=1, choices=[('X', 'X'), ('O', 'O')], null=True, blank=True)
+    game_over = models.BooleanField(default=False)
+
+    @property
+    def game_has_winner(self):
+        return self.winner is not None
+    
+    @property
+    def game_is_over(self):
+        return self.game_over
+
+    def __str__(self):
+        return str(self.move)
